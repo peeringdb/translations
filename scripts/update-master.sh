ffc884aa227d8a8a5ea7ae967a4a9a75f4220625
Matt Griswold <grizz@20c.com>
2024-12-01 15:50:11 -0600

    combine update scripts

--- scripts/update-master.sh
+++ scripts/update-master.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
-# clones PeeringDB projects, limited to the installed release tag, to be
+# takes arugments `--wlc` and `--commit`
+#
+# --wlc: use wlc to lock and unlock the projects
+#   requires a config file with API key at `~/.config/weblate`
+#
+# --commit: commit the changes to the translations repo
+#
+# This clones PeeringDB projects, limited to the installed release tag, to be
 # translated.  Adds to the translation files.  Meant to be run on beta and
 # prod master and NOT meant to have results committed to upstream repo, since
 # the upstream repo is expected to be based later tags.
+#
+# More docs are available in the deploy repo.
 #
 # This is best run with a fresh "git clone https://github.com/peeringdb/translations.git"
 # of the latest translations from the weblate server.  The idea is this:
@@ -25,6 +34,12 @@
 #
 
 MAKEMSG_OPTIONS="--all --symlinks --no-wrap --no-location --keep-pot"
+# use the pdb-container, which will mount $PWD to /mnt
+PDB_DJANGO_ADMIN="/home/pdb/bin/pdb-container"
+USE_WLC=false
+COMMIT=false
+WLC="wlc"
+WLC_LOCKED="false"
 
 # reset git repo to be origin default branch
 function reset_repository() {
@@ -38,6 +53,10 @@ function reset_repository() {
 
 function clean_up() {
     error_code=$? # this needs to be here to catch the intended exit code
+    if [ "$WLC_LOCKED" == "true" ]; then
+        $WLC unlock peeringdb/server
+        $WLC unlock peeringdb/javascript
+    fi
     set +e
     set +x
     echo
@@ -45,7 +64,87 @@ function clean_up() {
     exit $error_code
 }
 
+function wlc_lock() {
+    # Guidance from https://docs.weblate.org/en/latest/admin/continuous.html
+    #
+    # Exit if already locked:
+    echo
+    echo If locked, connect to trans0.peeringdb.com and figure out why. Unlock with:
+    echo "    $WLC unlock peeringdb/server"
+    echo "    $WLC unlock peeringdb/javascript"
+    $WLC lock-status peeringdb/server | grep True && exit 1
+    $WLC lock-status peeringdb/javascript | grep True && exit 1
+
+    # Lock weblate components:  (mild race condition here since not atomic)
+    WLC_LOCKED="true"
+    $WLC lock peeringdb/server || exit 1
+    $WLC lock peeringdb/javascript || exit 1
+
+    # tell weblate to push any changes to the repo
+    $WLC push
+}
+
+function commit_changes() {
+
+    server_head=$(git --git-dir=peeringdb/.git rev-parse --short HEAD)
+    django_head=$(git --git-dir=django-peeringdb/.git rev-parse --short HEAD)
+    django_auth_head=$(git --git-dir=django-oauth-toolkit/.git rev-parse --short HEAD)
+
+    # Remove "POT-Creation-Date:" lines from both .po and .pot files, so that we don't have a diff every run.
+    find $WORK_DIR -name *.po* -print0 | xargs -0 sed -i /^\"POT-Creation-Date:/d || exit 1
+
+    msg="new translations (server:$server_head django:$django_head django-oauth-toolkit:$django_auth_head)"
+
+    git add locale
+    # Deduce whether to perfom a commit:
+    [[ -z $(git status --untracked-files=no --porcelain) ]] || git commit -m "$msg"
+
+    echo $msg
+}
+
+# Parse optional arguments
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    --wlc) USE_WLC=true ;;
+    --commit) COMMIT=true ;;
+    *)
+        echo "Unknown parameter passed: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+# committing without lock can cause conflicts
+if [ "$COMMIT" == true ] && [ "$USE_WLC" != true ]; then
+    echo "--commit requires --wlc to be enabled"
+    exit 1
+fi
+
+# check for django-admin in path
+if ! command -v $PDB_DJANGO_ADMIN &>/dev/null; then
+    echo "django-admin could not be found"
+    exit 1
+fi
+
+# check for wlc requirements
+if [ "$USE_WLC" == true ]; then
+    WLC="wlc"
+    WLC_LOCKED="false"
+    if ! command -v $WLC &>/dev/null; then
+        echo "wlc could not be found"
+        exit 1
+    fi
+    if [ ! -f ~/.config/weblate ]; then
+        echo "wlc config file not found"
+        exit 1
+    fi
+fi
+
 trap clean_up EXIT
+
+echo "Running update-locale.sh with WLC=$WLC, COMMIT=$COMMIT"
 
 # exit on error
 set -e
@@ -57,7 +156,6 @@ if [ -d "/srv/translate.peeringdb.com" ]; then
 fi
 
 # Determine peeringdb.git version currently deployed:
-PDB_DJANGO_ADMIN="/home/pdb/bin/pdb-container"
 PDB_TAG=$(docker image inspect peeringdb_server:latest | grep -o "peeringdb_server:[0-9][^,]*" | cut -d ':' -f2 | sed 's/["|,]//g')
 
 # 20190602: Caputo hasn't figured out how to determine version of
@@ -84,6 +182,10 @@ else
     reset_repository "django-oauth-toolkit"
 fi
 
+if [ "$USE_WLC" == true ]; then
+    wlc_lock
+fi
+
 echo
 echo If \"duplicate message definition\" errors in the below, edit indicated file by removing duplicate that _does_not_ already have a translation, even if commented out. Then re-run $0 manually.
 echo
@@ -93,3 +195,9 @@ $PDB_DJANGO_ADMIN makemessages $MAKEMSG_OPTIONS
 $PDB_DJANGO_ADMIN makemessages $MAKEMSG_OPTIONS --domain djangojs
 $PDB_DJANGO_ADMIN compilemessages
 set +x
+
+if [ "$COMMIT" == true ]; then
+    commit_changes
+    # tell weblate to pull changes
+    $WLC pull
+fi
